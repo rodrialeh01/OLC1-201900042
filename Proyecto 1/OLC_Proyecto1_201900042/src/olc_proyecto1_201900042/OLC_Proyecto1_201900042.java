a50fa17e7cc8be9ee66753aa9a1fef15680730f3
rodrialeh01 <2823897250101@ingenieria.usac.edu.gt>
2022-08-20 12:09:32 -0600

    Creación del Proyecto e importacion de librerias JLex y JCup

---
+++ Proyecto 1/OLC_Proyecto1_201900042/src/olc_proyecto1_201900042/OLC_Proyecto1_201900042.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package olc_proyecto1_201900042;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class OLC_Proyecto1_201900042 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
